82a7c1db8aabde00be1b4813a041c8fbfd55e650
Laura Rodríguez <laurarguezfdez@gmail.com>
2024-06-15 12:52:07 +0200

    Ejercicios arrays

---
+++ 2_11_metodos_funcionales_arrays/2_11_1_map/2_11_1_ejercicio_1/js/main.js
@@ -0,0 +1,4 @@
+const marks = [5, 4, 6, 7, 9];
+
+const inflatedMarks = marks.map(marksElement => marksElement + 1);
+console.log(inflatedMarks);
---
+++ 2_11_metodos_funcionales_arrays/2_11_1_map/2_11_1_ejercicio_2/js/main.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const names = ['María', 'Lucía', 'Susana', 'Rocío', 'Inmaculada'];
+
+const saludos = names.map(namesElement => `Bienvenida ${namesElement}`);
+console.log(saludos);
